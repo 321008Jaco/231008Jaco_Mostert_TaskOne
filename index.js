8395977f976382c94084113b80500f8f9b5d0751
231008Jaco <231008@virtualwindow.co.za>
2023-05-02 12:19:33 +0200

    Explored
    Explored things in class

---
+++ index.js
@@ -0,0 +1,11 @@
+let susan = 56;
+let timmy = 126;
+let josh = 60;
+let win = false;
+
+let TeamST = susan + timmy;
+let victory = josh / 2;
+let answer = victory + TeamST;
+
+console.log(answer);
+console.log(win)
