daf627ce7b215bbc0460959ff15407e864b5c8d0
231008Jaco <231008@virtualwindow.co.za>
2023-05-08 23:00:49 +0200

    Ex One, Two and three
    Finished Exercises.

---
+++ js/index.js
@@ -0,0 +1,60 @@
+alert("Week 2 Homework!")
+
+var text = "ExerciseOne"
+
+let Name = "Jaco";
+let Surname = "Mostert";
+let Age = 20;
+let Subject = "Development";
+
+console.log(text)
+console.log(Name)
+console.log(Surname)
+console.log(Age)
+console.log(Subject)
+
+let message = "Hello, my name is " + Name + " " + Surname + ". I am " + Age + " years old and I am studying " + Subject + ".";
+
+console.log(message)
+
+
+
+
+
+let numOne = "4";
+let numTwo = "34";
+let numThree = "15";
+let numFour = "9";
+let answer = "0";
+
+let teamTotal = numTwo / numThree;
+let devide = teamTotal * numFour;
+let total = devide + teamTotal
+
+var end = answer + total;
+
+console. log(end)
+
+
+
+
+
+let Timmy = "R10";
+let Susan = "R25";
+
+let timmyTax = Timmy / 100 * 15;
+let susanTax = Susan / 100 * 15;
+let value = timmyTax + susanTax;
+
+var answ = value;
+
+console.log(answ)
+
+
+
+
+
+let John = "60";
+let Tim = "45";
+let Alfie = "80";
+let Gwen = "55";
